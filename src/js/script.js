03e5988f5b187ac1712b3578dc94bb38f685931e
Максим Сергеевич <krovorgen@gmail.com>
2021-01-30 09:54:53 +0300

    Complete footer

---
+++ src/js/script.js
@@ -0,0 +1,9 @@
+// prettier-ignore
+/* eslint-disable */
+$(function() {
+    $('.photo__slider').slick({
+        prevArrow: '<button type="button" class="slick-prev"><img src="img/left-arrow.svg" alt=""></button>',
+        nextArrow: '<button type="button" class="slick-next"><img src="img/right-arrow.svg" alt=""></button>',
+    });
+})
+/* eslint-enable */
